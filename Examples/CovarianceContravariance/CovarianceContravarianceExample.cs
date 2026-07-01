7aad4e65de53963f4e1fa1e74d87ea3ac1faace4
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2026-07-01 15:01:33 +0200

    Kowariancja i Kontrwariancja

--- Examples/CovarianceContravariance/CovarianceContravarianceExample.cs
+++ Examples/CovarianceContravariance/CovarianceContravarianceExample.cs
@@ -4,5 +4,18 @@ internal static class CovarianceContravarianceExample
 {
     public static void Run()
     {
+        IEnumerable<Car> cars = [new Audi()];
+        IEnumerable<Vehicle> vehicles = cars; // kowariancja 
+
+        IComparer<Vehicle> vehicleComparer =
+            Comparer<Vehicle>.Create((a, b) => string.Compare(a.Name, b.Name, StringComparison.Ordinal));
+        IComparer<Car> carComparer = vehicleComparer; // kontrwariancja
+
+        Console.WriteLine(vehicles.First().Name);
+        Console.WriteLine(carComparer.Compare(new Car(), new Audi()));
     }
 }
+
+class Vehicle { public virtual string Name => "vehicle"; }
+class Car : Vehicle { public override string Name => "car"; }
+class Audi : Car { public override string Name => "audi"; }
