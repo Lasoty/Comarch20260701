56395e94d0c3bf11a0ec9132247808a44704ec8e
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2026-07-01 12:23:08 +0200

    Wyrażenia lambda Metody rozszerzające.

--- Examples/ExtensionMethods/ExtensionMethodsExample.cs
+++ Examples/ExtensionMethods/ExtensionMethodsExample.cs
@@ -4,5 +4,26 @@ internal static class ExtensionMethodsExample
 {
     public static void Run()
     {
+        Console.WriteLine("Ala ma kota. Kot ma Alę?".WordCount());
     }
+}
+
+public static class StringExtensions
+{
+    extension(string text)
+    {
+        public int WordCount()
+        {
+            return text.Split([' ', '.', '?', '!'], StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        public string FirstLetterUpper()
+        {
+            return text.Insert(0, text[0].ToString().ToUpper()).Remove(1);
+        }
+    }
+
+    //public static string FirstLetterUpper(this string text)
+    //{
+    //    return text.Insert(0, text[0].ToString().ToUpper()).Remove(1);
+    //}
 }
--- Examples/LambdaClosuresExpressionTrees/LambdaClosuresExpressionTreesExample.cs
+++ Examples/LambdaClosuresExpressionTrees/LambdaClosuresExpressionTreesExample.cs
@@ -1,8 +1,19 @@
+using System.Linq.Expressions;
+
 namespace DotNet10ExamplesStarter.Examples.LambdaClosuresExpressionTrees;
 
 internal static class LambdaClosuresExpressionTreesExample
 {
     public static void Run()
     {
+        var threshold = 100m;
+
+        Func<Product, bool> compiled = p => p.Price >= threshold;
+        Expression<Func<Product, bool>> tree = p => p.Price >= threshold;
+
+        Console.WriteLine(compiled(new Product("Monitor", "black", 1200m)));
+        Console.WriteLine($"Drzewo: {tree.Body.NodeType}, {tree.Body}");
     }
 }
+
+public sealed record Product(string Name, string Color, decimal Price);
