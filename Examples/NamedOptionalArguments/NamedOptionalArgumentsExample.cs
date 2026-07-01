69d3e6e75c93bb72c6fbe080ba1b66b27a07ead0
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2026-07-01 14:49:55 +0200

    Parametry nazwane i opcjonalne

--- Examples/NamedOptionalArguments/NamedOptionalArgumentsExample.cs
+++ Examples/NamedOptionalArguments/NamedOptionalArgumentsExample.cs
@@ -4,5 +4,18 @@ internal static class NamedOptionalArgumentsExample
 {
     public static void Run()
     {
+        PrintOrderDetails(orderNumber: 31, productName: "Red mug", sellerName: "Gift shop", address: "Bydgoszcz");
+        PrintOrderDetails(productName: "Blue mug", orderNumber: 32);
+        PrintOrderDetails(productName: "Blue mug", orderNumber: 32, address: "Gdańsk");
+    }
+
+    private static void PrintOrderDetails(
+        int orderNumber,
+        string productName,
+        string sellerName = "Internet Shop",
+        string address = "N/A"
+    )
+    {
+        Console.WriteLine($"#{orderNumber}: {productName}, sprzedawca={sellerName}, address=[{address}]");
     }
 }
