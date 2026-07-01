9755ce94d9e61cec30843c59eb1662c492eea8b2
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2026-07-01 11:43:19 +0200

    Metody anonimowe Inicjatory objektów.

--- Examples/AnonymousMethodsAndLambdas/AnonymousMethodsAndLambdasExample.cs
+++ Examples/AnonymousMethodsAndLambdas/AnonymousMethodsAndLambdasExample.cs
@@ -4,5 +4,10 @@ internal static class AnonymousMethodsAndLambdasExample
 {
     public static void Run()
     {
+        Func<int, int> oldStyle = delegate (int x) { return x * x; };
+        Func<int, int> lambdaBlock = x => { return x * x; };
+        Func<int, int> lambdaExpression = x => x * x;
+
+        Console.WriteLine($"{oldStyle(5)}, {lambdaBlock(6)}, {lambdaExpression(7)}");
     }
 }
--- Examples/ObjectAndCollectionInitializers/ObjectAndCollectionInitializersExample.cs
+++ Examples/ObjectAndCollectionInitializers/ObjectAndCollectionInitializersExample.cs
@@ -4,5 +4,19 @@ internal static class ObjectAndCollectionInitializersExample
 {
     public static void Run()
     {
+        Cat cat = new Cat() { Age = 10, Name = "Fluffy" };
+        Cat cat2 = new() { Age = 10, Name = "Fluffy" };
+
+        List<Cat> cats = new List<Cat>() { cat, cat2, new() { Age = 14, Name = "Sasha" } };
+        List<Cat> cats2 = [cat, cat2, new() { Age = 14, Name = "Sasha" }];
+
+        Dictionary<int, string> names = new() { [7] = "seven", [9] = "nine" };
     }
+}
+
+
+public sealed record Cat
+{
+    public string Name { get; init; } = "";
+    public int Age { get; init; }
 }
