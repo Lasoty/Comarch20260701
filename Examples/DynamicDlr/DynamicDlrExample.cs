a80f6399fa1cf72e37fcb570d5070236235df3c6
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2026-07-01 15:20:31 +0200

    typy dynamiczne

--- Examples/DynamicDlr/DynamicDlrExample.cs
+++ Examples/DynamicDlr/DynamicDlrExample.cs
@@ -1,8 +1,17 @@
+using System.Dynamic;
+
 namespace DotNet10ExamplesStarter.Examples.DynamicDlr;
 
 internal static class DynamicDlrExample
 {
     public static void Run()
     {
+        dynamic invoice = new ExpandoObject();
+
+        invoice.Net = 100m;
+        invoice.Vat = 0.23m;
+        invoice.Gross = new Func<decimal>(() => invoice.Net * (1 + invoice.Vat));
+
+        Console.WriteLine($"dynamic gross={invoice.Gross():0.00}");
     }
 }
